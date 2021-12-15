20e819c15f301e382b42b23fb3bee104aaac2d5a
chrisdickerman93@gmail.com <74125686+Slidpirat@users.noreply.github.com>
2021-12-14 18:59:10 -0700

    first commit

---
+++ data-python/data_presenter.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+data = open('../CupcakeInvoices.csv')
+
+for row in data:
+    print(row)
+
+for row in data:
+    values = row.split(',')
+    print(values[2])
+
+for row in data:
+    values = row.split(',')
+    total = int(values[3]) * float(values[4])
+    print(total)
+
+total = 0
+
+for row in data:
+    values = row.split(',')
+    total = total + (int(values[3]) * float(values[4]))
+
+print(total)
+
+data.close()
+
+
+x = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
+
+y = [10, 40, 32, 84, 60, 52, 18]
+
+plt.plot(x, y)
+plt.xlabel('Day Purchased')
+plt.ylabel('Cupcakes Purchased')
+plt.title('My Cupcake Sales')
+plt.show()
